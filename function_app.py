757451eafe1a905ad6b11cb809077894db4bbca4
joycejemimas <21f1006131@ds.study.iitm.ac.in>
2024-12-29 01:43:51 +0530

    Func_app edit

--- function_app.py
+++ function_app.py
@@ -6,9 +6,11 @@ import logging
 app = func.FunctionApp()
 
 
-@app.blob_trigger(arg_name="myblob", path="mycontainer",
+@app.blob_trigger(arg_name="myblob", path="mycontainer/",
                                connection="AzureWebJobsStorage") 
 def ResizeImageFunction(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
+
+
